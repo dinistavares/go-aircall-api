6c8764ad1242a08ad35e9932502d4a8fb3bb6e10
dinistavares <dinis@crisp.chat>
2024-11-18 12:22:29 +0000

    use interface instead

--- contact.go
+++ contact.go
@@ -29,7 +29,7 @@ type CreateUpdateContact struct {
 }
 
 type Contact struct {
-	ID           int            `json:"id,omitempty"`
+	ID           interface{}    `json:"id,omitempty"`
 	FirstName    string         `json:"first_name,omitempty"`
 	LastName     string         `json:"last_name,omitempty"`
 	CompanyName  string         `json:"company_name,omitempty"`
